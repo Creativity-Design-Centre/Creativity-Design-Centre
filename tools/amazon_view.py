12ab742e7b12fe03b4012f5f692864821ca0ea22
lee920217 <francislee920217@gmail.com>
2020-11-25 14:08:41 +0800

    add sentiment

--- tools/amazon_view.py
+++ tools/amazon_view.py
@@ -8,11 +8,11 @@ from urllib.parse import urlparse, urlencode
 
 def get_base_data(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-
-    cookies = {
-       
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding": "gzip, deflate", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"
     }
+
+    cookies = {"session-id": "262-6285588-4935355", " i18n-prefs": "GBP", " ubid-acbuk": "257-2158496-9450536", " lc-acbuk": "en_GB",
+               " session-token": "FwK5lvWuF0KZWniIBw1PQywMFlZ+RLLLfp4z5oIhvIrCFixTOv7Qx08S/r/qy5RKTCEaD6OkfGbFe4mE+dKH4NdPVCR40hj94cJmRh34Q164HHuiITyA4Fse6BFl9rZ7pvv+o69gcbItLmxfOHkrgYNdRODMJ578tLSAHGwOar1aa9ct8Y2EsOIQU+PLRACX", " session-id-time": "2082758401l", " csm-hit": "tb:s-3KBEGE51Y5H40ZJ52X4S|1602491049203&t:1602491051320&adb:adblk_no"}
     html_content = requests.get(
         url, "html.parser", headers=headers, cookies=cookies)
     soup_content = BeautifulSoup(html_content.text, features="lxml")
@@ -21,12 +21,26 @@ def get_base_data(url):
 
 def get_review_count(_url):
     soup_content = get_base_data(_url)
-    print(soup_content)
     review_count = soup_content.find(
-        "span", attrs={"data-hook": "cr-filter-info-review-count"})
-    all_count = re.findall(r"\d+\.?\d*",
-                           review_count.text.split('of')[1])[0]
-    return int(all_count)
+        "div", attrs={"data-hook": "cr-filter-info-review-rating-count"})
+    review_count = review_count.find('span').text
+    review_count = review_count.replace(',', '')
+    review_count = review_count.split('|')[1]
+    review_count = re.findall(r"\d+\.?\d*", review_count)
+    #print(int(review_count[0]))
+    return (int(review_count[0]))
+    # all_count = re.findall(r"\d+\.?\d*",
+    #                        review_count.text.split('of')[1])[0]
+    # return int(all_count)
+
+
+def filter_emoji(desstr, restr=''):
+    # 过滤表情
+    try:
+        co = re.compile(u'[\U00010000-\U0010ffff]')
+    except re.error:
+        co = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+    return co.sub(restr, desstr)
 
 
 def get_data(_url):
@@ -40,8 +54,10 @@ def get_data(_url):
     review_content = soup_content.find_all(
         "span", attrs={"class": "review-text"})
     for i in review_content:
-        tmp_save.append(i.find("span").text + '\n')
-        print('get_data', i.find("span").text)
+        #print('=====<<<<<<<', i.find("span"), i.find("span") is not None)
+        if i.find("span") is not None:
+            tmp_save.append(filter_emoji(i.find("span").text) + '\n')
+            #print('get_data', i.find("span").text)
     return tmp_save
     print('----------------> finishing')
 # print(r)
@@ -53,28 +69,34 @@ if __name__ == "__main__":
     addurl = _url
     file_name = 'product-review' + addurl.split('/')[4]
     print(get_review_count(_url))
-    pages = int(get_review_count(_url) / 10) - 1
-    print(pages)
+    pages = int(get_review_count(_url) / 10) + 1
+    _parse = urlparse(_url)
+    _query = _parse.query
+    _query_spilt = _query.split('&')
+    i = 0
+    _new_params = {}
+    for j in _query_spilt:
+        key = j.split('=')[0]
+        value = j.split('=')[1]
+        _new_params[key] = value
+    if 'pageNumber' in _new_params:
+        i = _new_params['pageNumber']
+        #print(i)
+    else:
+        i = 1
+        _new_params['pageNumber'] = 1
+    print(_new_params)
 
-    for i in range(int(pages)):
-        # set init page number
-        i = i + 1
+    for i in range(int(i), int(pages)):
         # pars input url
-        _parse = urlparse(_url)
-        _query = _parse.query
-        _query_spilt = _query.split('&')
-        _new_params = {}
-        for j in _query_spilt:
-            key = j.split('=')[0]
-            value = j.split('=')[1]
-            _new_params[key] = value
-        _new_params['pageNumber'] = i
-        print(_new_params)
         _new_url = _parse.scheme + '://' + _parse.netloc + \
             _parse.path + '?' + urlencode(_new_params)
         # addurl = _new_url
         print('===>', _new_url)
         loop_data = get_data(_new_url)
-        review_file = open('./' + file_name + '.txt', 'a')
+        review_file = open('./' + file_name + '-' +
+                           _new_params['filterByStar'] + '.txt', 'a')
         review_file.writelines(loop_data)
-        time.sleep(10)
+        i = i + 1
+        _new_params['pageNumber'] = i
+        time.sleep(3)
---
+++ tools/sentiment_analysis/sentiment.py
@@ -0,0 +1,242 @@
+from textblob import TextBlob
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.feature_extraction.text import HashingVectorizer
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from itertools import combinations, chain
+import numpy as np
+import pyLDAvis
+import math
+
+positive_list = open('../product-reviewB07H625JJL-five_star.txt',  'r')
+
+new_list = []
+
+
+class oBTM:
+    """ Biterm Topic Model
+
+        Code and naming is based on this paper http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.402.4032&rep=rep1&type=pdf
+        Thanks to jcapde for providing the code on https://github.com/jcapde/Biterm
+    """
+
+    def __init__(self, num_topics, V, alpha=1., beta=0.01, l=0.5):
+        self.K = num_topics
+        self.V = V
+        self.alpha = np.full(self.K, alpha)
+        self.beta = np.full((len(self.V), self.K), beta)
+        self.l = l
+
+    def _gibbs(self, iterations):
+
+        Z = np.zeros(len(self.B), dtype=np.int8)
+        n_wz = np.zeros((len(self.V), self.K), dtype=int)
+        n_z = np.zeros(self.K, dtype=int)
+
+        for i, b_i in enumerate(self.B):
+            topic = np.random.choice(self.K, 1)[0]
+            n_wz[b_i[0], topic] += 1
+            n_wz[b_i[1], topic] += 1
+            n_z[topic] += 1
+            Z[i] = topic
+
+        for _ in range(iterations):
+            for i, b_i in enumerate(self.B):
+                n_wz[b_i[0], Z[i]] -= 1
+                n_wz[b_i[1], Z[i]] -= 1
+                n_z[Z[i]] -= 1
+                P_w0z = (n_wz[b_i[0], :] + self.beta[b_i[0], :]
+                         ) / (2 * n_z + self.beta.sum(axis=0))
+                P_w1z = (n_wz[b_i[1], :] + self.beta[b_i[1], :]) / \
+                    (2 * n_z + 1 + self.beta.sum(axis=0))
+                P_z = (n_z + self.alpha) * P_w0z * P_w1z
+                # P_z = (n_z + self.alpha) * ((n_wz[b_i[0], :] + self.beta[b_i[0], :]) * (n_wz[b_i[1], :] + self.beta[b_i[1], :]) /
+                #                            (((n_wz + self.beta).sum(axis=0) + 1) * (n_wz + self.beta).sum(axis=0)))  # todo check out
+                P_z = P_z / P_z.sum()
+                Z[i] = np.random.choice(self.K, 1, p=P_z)
+                n_wz[b_i[0], Z[i]] += 1
+                n_wz[b_i[1], Z[i]] += 1
+                n_z[Z[i]] += 1
+
+        return n_z, n_wz
+
+    def fit_transform(self, B_d, iterations):
+        self.fit(B_d, iterations)
+        return self.transform(B_d)
+
+    def fit(self, B_d, iterations):
+        self.B = list(chain(*B_d))
+        n_z, self.nwz = self._gibbs(iterations)
+
+        self.phi_wz = (self.nwz + self.beta) / \
+            np.array([(self.nwz + self.beta).sum(axis=0)] * len(self.V))
+        self.theta_z = (n_z + self.alpha) / (n_z + self.alpha).sum()
+
+        self.alpha += self.l * n_z
+        self.beta += self.l * self.nwz
+
+    def transform(self, B_d):
+
+        P_zd = np.zeros([len(B_d), self.K])
+        for i, d in enumerate(B_d):
+            P_zb = np.zeros([len(d), self.K])
+            for j, b in enumerate(d):
+                P_zbi = self.theta_z * \
+                    self.phi_wz[b[0], :] * self.phi_wz[b[1], :]
+                P_zb[j] = P_zbi / P_zbi.sum()
+            P_zd[i] = P_zb.sum(axis=0) / P_zb.sum(axis=0).sum()
+
+        return P_zd
+
+
+def vec_to_biterms(X):
+    B_d = []
+    for x in X:
+        b_i = [b for b in combinations(np.nonzero(x)[0], 2)]
+        B_d.append(b_i)
+    return B_d
+
+
+def topic_summuary(P_wz, X, V, M, verbose=True):
+    res = {
+        'coherence': [0] * len(P_wz),
+        'top_words': [[None]] * len(P_wz)
+    }
+    for z, P_wzi in enumerate(P_wz):
+        V_z = np.argsort(P_wzi)[:-(M + 1):-1]
+        W_z = V[V_z]
+
+        # calculate topic coherence score -> http://dirichlet.net/pdf/mimno11optimizing.pdf
+        C_z = 0
+        for m in range(1, M):
+            for l in range(m):
+                D_vmvl = np.in1d(np.nonzero(X[:, V_z[l]]), np.nonzero(
+                    X[:, V_z[m]])).sum(dtype=int) + 1
+                D_vl = np.count_nonzero(X[:, V_z[l]])
+                if D_vl is not 0:
+                    C_z += math.log(D_vmvl / D_vl)
+
+        res['coherence'][z] = C_z
+        res['top_words'][z] = W_z
+        if verbose:
+            print('Topic {} | Coherence={:0.2f} | Top words= {}'.format(
+                z, C_z, ' '.join(W_z)))
+    return res
+
+
+def transform(dataset, n_features=1000):
+    vectorizer = TfidfVectorizer(
+        max_df=0.5, max_features=n_features, min_df=2, use_idf=True)
+    X = vectorizer.fit_transform(dataset)
+    return X, vectorizer
+
+
+def output_train(X, vectorizer, true_k=10, minibatch=False, showLable=False):
+    if minibatch:
+        km = MiniBatchKMeans(n_clusters=true_k, init='k-means++', n_init=1,
+                             init_size=1000, batch_size=1000, verbose=False)
+    else:
+        km = KMeans(n_clusters=true_k, init='k-means++', max_iter=300, n_init=1,
+                    verbose=False)
+    km.fit(X)
+    cluster_1 = []
+    cluster_2 = []
+    cluster_3 = []
+    cluster1_semantic = 0
+    cluster2_semantic = 0
+    cluster3_semantic = 0
+
+    if showLable:
+        print("Top terms per cluster:")
+        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+        terms = vectorizer.get_feature_names()
+        print(vectorizer.get_stop_words())
+        for i in range(true_k):
+            print("Cluster %d:" % i, end='')
+            for ind in order_centroids[i, :10]:
+                print(' %s' % terms[ind], end='')
+            print()
+    result = list(km.predict(X))
+    for j in range(len(result)):
+        if result[j] == 0:
+            cluster_1.append(new_list[j])
+        # elif result[j] == 1:
+        #     cluster_2.append(new_list[j])
+        # elif result[j] == 2:
+        #     cluster_3.append(new_list[j])
+    for k in cluster_1:
+        cluster1_semantic = cluster1_semantic + TextBlob(k).sentiment[0]
+
+    vec = CountVectorizer(stop_words='english')
+    X1 = vec.fit_transform(cluster_1)
+    vocab = np.array(vec.get_feature_names())
+    biterms = vec_to_biterms(X1)
+    btm = oBTM(num_topics=10, V=vocab)
+    print("\n\n Train Online BTM ..")
+    for i in range(0, len(biterms), 100):  # prozess chunk of 200 texts
+        biterms_chunk = biterms[i:i + 100]
+        btm.fit(biterms_chunk, iterations=50)
+    topics = btm.transform(biterms)
+    print("\n\n Visualize Topics ..")
+    vis = pyLDAvis.prepare(btm.phi_wz.T, topics, np.count_nonzero(
+        X, axis=1), vocab, np.sum(X, axis=0))
+    pyLDAvis.save_html(vis, './vis/online_btm.html')  # path to output
+
+    print("\n\n Topic coherence ..")
+    topic_summuary(btm.phi_wz.T, X, vocab, 10)
+    print("\n\n Texts & Topics ..")
+    for i in range(len(cluster_1)):
+        print("{} (topic: {})".format(cluster_1[i], topics[i].argmax()))
+    topics_file = open('./topic.text', 'a')
+    topics_file.writelines(topics)
+    return -km.score(X)
+
+
+def open_file():
+    file_list = ['../product-reviewB07H625JJL-one_star.txt', '../product-reviewB07H625JJL-two_star.txt',
+                 '../product-reviewB07H625JJL-three_star.txt', '../product-reviewB07H625JJL-four_star.txt', '../product-reviewB07H625JJL-five_star.txt']
+    for i in file_list:
+        with open(i,  'r') as f:
+            content = f.readlines()
+            all_comment = 0
+            positive_comment = 0
+            for i in content:
+                if i != '\n':
+                    all_comment += 1
+                    new_list.append(i)
+    # print(len(new_list))
+    X, vectorizer = transform(new_list, n_features=500)
+    output_train(X, vectorizer, true_k=3)
+
+
+open_file()
+
+# with open('../product-reviewB07H625JJL-one_star.txt',  'r') as f:
+#     content = f.readlines()
+#     all_comment = 0
+#     positive_comment = 0
+#     for i in content:
+#         if i != '\n':
+#             all_comment += 1
+#             new_list.append(i)
+#     for j in new_list:
+#         if TextBlob(j).sentiment[0] > 0:
+#             positive_comment += 1
+#     X, vectorizer = transform(new_list, n_features=500)
+#     true_ks = []
+#     scores = []
+#     for i in range(3, 80, 1):
+#         score = output_train(X, vectorizer, true_k=i)
+#         print(i, score)
+#         true_ks.append(i)
+#         scores.append(score)
+#     plt.figure(figsize=(8, 4))
+#     plt.plot(true_ks, scores, label="score", color="blue", linewidth=1)
+#     plt.xlabel("n_features")
+#     plt.ylabel("blue")
+#     plt.legend()
+#     plt.show()
+# out = output_train(X, vectorizer, true_k=10, showLable=True)
+# print(out)
+# print(all_comment, positive_comment)
